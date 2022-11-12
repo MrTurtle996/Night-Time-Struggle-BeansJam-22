d40761ae5db3b96035a76b06a263547fd6d2453a
MrTurtle996 <49366964+MrTurtle996@users.noreply.github.com>
2022-11-12 16:33:01 +0100

    Health Bar

--- Scenes/SleepBar.gd
+++ Scenes/SleepBar.gd
@@ -7,11 +7,15 @@ signal game_end()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var _connect = get_node("../../Character").connect("player_is_moving", self, "_on_emitter_player_is_moving")
+	_connect = get_node("../../Hover").connect("place_tile", self, "_on_emitter_place_tile")
 
 func _display_health():
 	var children = get_children()[sleep_bar - 1]
 	children.hide()
-	
+
+func _on_emitter_place_tile():
+	_display_health()
+	sleep_bar -= 1
 
 func _on_emitter_player_is_moving(is_moving):
 	if is_moving:
